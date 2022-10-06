b2f31f1a83f5250a95ca7928b84aa7e464c5fbe3
thswodn1234 <thswodn1234@naver.com>
2022-10-06 16:14:05 +0900

    project1ToComponent

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <nav>
+    <div className="nav_logo">
+      <p>mylogo</p>
+    </div>
+    <ul className="nav_menu">
+      <li>
+        <a href="">HTML</a>
+      </li>
+      <li>
+        <a href="">CSS</a>
+      </li>
+      <li>
+        <a href="">JavaScript</a>
+      </li>
+      <li>
+        <a href="">React</a>
+      </li>
+      <li>
+        <a href="">facebook</a>
+      </li>
+      <li>
+        <a href="">instagram</a>
+      </li>
+    </ul>
+    <ul class="nav_icon">
+      <li>
+        <i class="fa-brands fa-facebook"></i>
+      </li>
+      <li>
+        <i class="fa-brands fa-square-instagram"></i>
+      </li>
+    </ul>
+  </nav>
+  );
+}
+
+export default App;
---
+++ src/App2.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App2() {
+  return (
+    <header>
+        <section>
+            <div class="header_left">
+                <h1>프론트엔드
+                    FrontEnd</h1>
+            </div>
+            <div class="header_right">
+                <p>웹에서 사용자에게 시각적으로 보여지는 부분을 의미한다. 프론트엔드쪽에서는 주로 HTML, CSS, 자바스크립트가 쓰이며, 요즘은 페이스북에서 개발한 자바스크립트에서 파생된 싱글
+                    페이지 애플리케이션 형태의 리액트가 많이 사용된다.</p>
+            </div>
+        </section>
+    </header>
+  );
+}
+
+export default App2;
