dc7380aec44643de60a750f3a09cf2b51d7b0858
thswodn1234 <thswodn1234@naver.com>
2022-10-06 16:48:40 +0900

    1006-2\

---
+++ src/Main3.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function Main3() {
+  return (
+    <section>
+      <div>
+        <img src="./images/js.png" alt="JavaScript" />
+      </div>
+      <div>
+        <h1>JavaScript</h1>
+        <p>
+          웹 페이지를 위한 스크립트 언어로 가벼운, 인터프리터 혹은 just-in-time
+          컴파일 프로그래밍 언어로, 일급 함수를 지원
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Main3;
